ae3bee0f2deb6040055eb0565d0d56d72716cffb
Umair <umairwaraich2019@gmail.com>
2024-03-19 16:20:50 +0500

    update stipe api version

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 const app = express();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2023-08-16",
 });
 
 app.use(
