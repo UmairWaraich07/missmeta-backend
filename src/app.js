0b98ed80a107a65eaed20a2ddebd516d962b5cae
Umair <umairwaraich2019@gmail.com>
2024-03-23 15:03:08 +0500

    add headers

--- src/app.js
+++ src/app.js
@@ -13,21 +13,10 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     ethods: "*",
-    // allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://frontend-missmeta.vercel.app");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  next();
-});
 
 app.use(
   express.json({
